198bb937b48df352798a00b948f68b21c857b5fc
gustavoloppes <124110090+gustavoloppes@users.noreply.github.com>
2023-04-10 09:36:48 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using DesafioPOO.Models;
+
+Nokia nokia = new Nokia (numero: "9999999", modelo: "N1", imei: "1234", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("TELEGRAM");
+
+Iphone iphone = new Iphone (numero: "888888", modelo: "14pro", imei: "4321", memoria: 518);
+iphone.Ligar();
+iphone.InstalarAplicativo("WHATSAPP");
